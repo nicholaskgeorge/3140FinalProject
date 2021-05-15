14b8fc5203ed6d3570026ce947a3eed48549bb99
Nicholas George <56926399+nicholaskgeorge@users.noreply.github.com>
2021-05-15 12:41:02 -0400

    Worked on accelerometer and python files. Serial read is causing hanging

--- testgame.py
+++ testgame.py
@@ -1,58 +1,57 @@
 # import pygame module in this program
 import pygame
+import random
+import serial
+import time
 
-#pick random number 0-3 (0 straight, 1 right, 2 left, 3 jump)
-random.randint(0, 3)
-
-# activate the pygame library .
-# initiate pygame and give permission
-# to use pygame's functionality.
 pygame.init()
-
 # define the RGB value
 # for white colour
 white = (255, 255, 255)
-
 # assigning values to X and Y variable
 X = 1500
 Y = 1000
-
 # create the display surface object
 # of specific dimension..e(X, Y).
 display_surface = pygame.display.set_mode((X, Y ))
-
 # set the pygame window name
 pygame.display.set_caption('GAME TIME')
-
-# create a surface object, image is drawn on it.
 image = pygame.image.load('cher.jpeg').convert()
+# create a surface object, image is drawn on it.
 #C:\Users\julietdenapoli\Desktop\
 # infinite loop
-while True :
+#pick random number 0-3 (0 straight, 1 right, 2 left, 3 jump)
+initial = 0
+ser = serial.Serial('COM5', 57600)
 
-    # completely fill the surface object
-    # with white colour
+while(1):
+    time.sleep(1)
+    data = ser.readline()
+    print("got here")
+    if initial == 0:
+        if(data == "ready"):
+            inital=1
+            ser.write("setgo")
+    else:
+        pickImage = random.randint(0,1)
+        ser.write(pickImage)
+        if pickImage == 1:
+            image = pygame.image.load('cher.jpeg').convert()
+        else:
+            image = pygame.image.load('cher.jpeg').convert()
+
     display_surface.fill(white)
-
-    # copying the image surface object
-    # to the display surface object at
-    # (0, 0) coordinate.
     display_surface.blit(image, (0, 0))
-
     # iterate over the list of Event objects
     # that was returned by pygame.event.get() method.
     for event in pygame.event.get() :
-
         # if event object type is QUIT
         # then quitting the pygame
         # and program both.
         if event.type == pygame.QUIT :
-
             # deactivates the pygame library
             pygame.quit()
-
             # quit the program.
             quit()
-
         # Draws the surface object to the screen.
         pygame.display.update()
